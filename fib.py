e81eb69287d274aece3f6ab1192d95226405dd81
pdraghis <pdraghis@stanford.edu>
2016-04-12 07:58:11 +0300

    I finished my lab!

--- fib.py
+++ fib.py
@@ -25,14 +25,15 @@ Usage
   - './fib.py help': display this help message.
 
 """
-        print(help_message)
+        print help_message
     elif args[0] == "approx" and len(args) == 2:
         phi_approx(int(args[1]))
     elif args[0] == "converge" and len(args) == 1:
         phi_converge()
     else:
-        print("Error: input not understood.\n" \
-                "    Type './fib.py help' for info on this program.")
+        print "Error: input not understood.\n" \
+                "    Type './fib.py help' for info on this program."
+
 def fib(n):
     """Return nth element of the Fibonacci sequence."""
     # Create the base case
@@ -54,11 +55,11 @@ phi_approx_output_format = \
 
 def phi_approx(n, show_output=True):
     """Return the nth-order Fibonacci approximation to the golden ratio."""
-    fib_n = fibbb(n)
+    fib_n = fib(n)
     fib_nm1 = fib(n - 1)
     phi = float(fib_n)/fib_nm1
     if show_output:
-        print(phi_approx_output_format.format(n, fib_n, fib_nm1, phi))
+        print phi_approx_output_format.format(n, fib_n, fib_nm1, phi)
     return phi
 
 phi_converge_output_format = \
@@ -69,14 +70,15 @@ phi_converge_output_format = \
 def phi_converge():
     """Keep calculating higher-order Fibonacci approximations to the golden
     ratio until it stops changing (to floating-point precision)."""
-
+    f = open('output.txt','w')
     i = 3
     phi_old = phi_approx(i - 1, show_output=False)
-    phi_new = phi_approx(i)
+    phi_new = phi_approx(i, show_output=False)
     while phi_old != phi_new:
         i += 1
         phi_old = phi_new
         phi_new = phi_approx(i, show_output=False)
-        print(phi_converge_output_format.format(i, phi_new, phi_old))
-    print("\nConverged to %.25f" % phi_new)
+        f.write (phi_converge_output_format.format(i, phi_new, phi_old))
+    f.write ("\nConverged to %.25f" % phi_new)
+    f.close()
 if __name__ == '__main__': main()
